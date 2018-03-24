b5fb7c32e6b959bcc4f56a590d2414dcd7b6efb7
JagdCake <jc@jagdcake.com>
2018-03-24 13:39:47 +0200

    Add status messages

--- transferFiles.sh
+++ transferFiles.sh
@@ -11,7 +11,12 @@ cd $dir
 exclude=data
 
 # archive everything that is not hidden and not excluded
+start=`date +%s`
 tar --exclude=$exclude -cvzf $app_name.tar.gz *
+end=`date +%s`
+creation_time=$((end-start))
+
+echo -e "\nArchive created in $creation_time sec.\n"
 
 # if the VPS username is different than the local one create an environment variable 
 user=`whoami`
@@ -23,9 +28,18 @@ ssh $user@$VPSIP mkdir -p $destination'logs'
 
 scp $app_name.tar.gz $user@$VPSIP:$destination
 
+echo
+
 rm $app_name.tar.gz
 
+start=`date +%s`
 # extract the archive in the app dir
 ssh $user@$VPSIP tar -xvzf $destination"$app_name".tar.gz -C $destination
+end=`date +%s`
+extract_time=$((end-start))
+
+echo -e "\nArchive extracted in $extract_time sec.\n"
 
 ssh $user@$VPSIP rm $destination"$app_name".tar.gz
+
+echo -e "Done\n"
