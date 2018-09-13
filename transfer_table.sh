913931b6775474d0b687f7f56c1e4649e4affd73
JagdCake <jagdcake@outlook.com>
2018-09-13 16:49:56 +0300

    Fix wrong prompt instructions

--- transfer_table.sh
+++ transfer_table.sh
@@ -4,9 +4,9 @@
 set -e
 
 dump_table() {
-    echo -e "Dump table for transfer to docker container\n" 
+    echo -e "Dump table for transfer to docker container\n"
 
-    read -p "Dump: (table name) " table_name
+    read -p "Table: (table name) " table_name
     read -p "From: (database name) " from_db_name
     read -e -p "To: (path to save dump file in) " path_to_dump
 
