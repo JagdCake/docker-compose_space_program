415893356b88ee6a2e4d4575199332585188d7ca
JagdCake <jagdcake@outlook.com>
2018-09-13 17:51:39 +0300

    Add function to transfer table to container database

--- transfer_table.sh
+++ transfer_table.sh
@@ -13,3 +13,24 @@ dump_table() {
     pg_dump -t "$table_name" "$from_db_name" > "$path_to_dump"/dump
 }
 
+transfer_table() {
+    echo -e "Copy table dump to docker container\n"
+
+    read -e -p "App: (path to dockerized app) " app_dir
+    read -p "Container: (container name) " container_name
+    read -p "Container database: (container database name) " container_database
+
+    cd "$app_dir"
+
+    docker.compose build || docker-compose build
+    docker.compose up -d || docker-compose up -d
+
+    docker cp "$path_to_dump"/dump "$container_name":/home/
+    docker exec -it "$container_name" sh -c "psql -h localhost -U "${container_database}" < /home/dump"
+    docker exec -it "$container_name" rm /home/dump
+
+    rm "$path_to_dump"/dump
+
+    docker.compose down || docker-compose down
+}
+
